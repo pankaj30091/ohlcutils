9a836d005839b775feef1a1b703c13ae2da6b9e0
Pankaj Sharma <pankajsh@hotmail.com>
2025-06-26 10:08:04 +0530

    v0.1.9

--- ohlcutils/__init__.py
+++ ohlcutils/__init__.py
@@ -4,7 +4,7 @@ import sys
 from importlib.resources import files
 from logging.handlers import TimedRotatingFileHandler
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 from .config import is_config_loaded, load_config
 
