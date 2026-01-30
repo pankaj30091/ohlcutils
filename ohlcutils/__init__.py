de391bbd2c7a5a8f37a4aaf35d6b2bd4a1ca4684
Pankaj <your.email@example.com>
2026-01-30 23:18:07 +0530

    Bump version to 0.1.13

--- ohlcutils/__init__.py
+++ ohlcutils/__init__.py
@@ -6,7 +6,7 @@ from importlib.resources import files
 from logging.handlers import TimedRotatingFileHandler
 from typing import Optional, List
 
-__version__ = "0.1.11"
+__version__ = "0.1.13"
 
 from .config import is_config_loaded, load_config
 
